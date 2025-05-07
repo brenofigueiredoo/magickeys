a557e341d705a152987fc87f37015046351d8ee4
unknown <brenos93@gmail.com>
2025-05-06 22:14:20 -0300

    fix

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import jsonServer from "json-server";
 import cors from "cors";
-import express from "express";
 import usuariosRouter from "./routes/usuarios.js";
 import desafiosRouter from "./routes/desafios.js";
 import licoesRouter from "./routes/licoes.js";
@@ -8,6 +7,7 @@ import musicasRouter from "./routes/musicas.js";
 import musicasFavoritasRouter from "./routes/musicas_favoritas.js";
 import progressoDesafiosRouter from "./routes/progresso_desafios.js";
 import progressoLicoesRouter from "./routes/progresso_licoes.js";
+import express from "express";
 import path from "node:path";
 
 const server = jsonServer.create();
@@ -18,7 +18,6 @@ server.use(cors());
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-// Configuração para servir arquivos estáticos da pasta uploads
 server.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 server.use(usuariosRouter);
